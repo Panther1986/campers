d68baab93db3bdec2be202a9fb5b0721b2910516
Olha Meshcheriakova <polzovatel77@gmail.com>
2024-08-15 01:34:51 +0300

    update1

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -1,20 +1,65 @@
 import css from "./CarCard.module.css";
-const CarCard = ({ items }) => {
+import sprite from "../../images/icons.svg";
+import { IoLocationOutline } from "react-icons/io5";
+import { FiWind } from "react-icons/fi";
+import { LiaBedSolid, LiaGasPumpSolid } from "react-icons/lia";
+import { TbToolsKitchen2, TbAutomaticGearbox } from "react-icons/tb";
+import { BsPeople } from "react-icons/bs";
+const CarCard = ({ item }) => {
+  const iconStar = `${sprite}#icon-Rating`;
   return (
     <ul className={css.ul}>
-      {items.map((item) => (
-        <>
-          <li key={item.id} className={css.li}>
-            <div>
-              <img src={item.gallery[0]} alt={`${item.name}`} width="130" />
-            </div>
-            <div className={css.name}>
-              <p>{item.name}</p>
-              <p>${item.price}.00</p>
-            </div>
-          </li>
-        </>
-      ))}
+      <>
+        <li key={item.id} className={css.li}>
+          <div>
+            <img src={item.gallery[0]} alt={`${item.name}`} width="130" />
+          </div>
+          <div className={css.name}>
+            <p>{item.name}</p>
+            <p>${item.price}.00</p>
+          </div>
+          <div>
+            <svg width="16" height="16">
+              <use href={iconStar}></use>
+            </svg>
+            <p>{item.rating}</p>
+            <p>
+              <IoLocationOutline />
+              {item.location}
+            </p>
+          </div>
+          <div>
+            <p className={css.description}>{item.description}</p>
+          </div>
+          <div>
+            <p>
+              <BsPeople />
+              {item.adults}
+            </p>
+            <p>
+              <TbAutomaticGearbox />
+              {item.transmission}
+            </p>
+            <p>
+              <LiaGasPumpSolid />
+              {item.engine}
+            </p>
+            <p>
+              <TbToolsKitchen2 />
+              {item.details.kitchen}
+            </p>
+            <p>
+              <LiaBedSolid />
+              {item.details.beds}
+            </p>
+            <p>
+              <FiWind />
+              {item.details.airConditioner}
+            </p>
+          </div>
+          <button className={css.btn}>Show more</button>
+        </li>
+      </>
     </ul>
   );
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,37 +2,64 @@ import { useEffect, useState } from "react";
 import { fetchAllCars } from "../../api.js";
 import toast from "react-hot-toast";
 import CarCard from "../../components/CarCard/CarCard.jsx";
+import css from "./HomePage.module.css";
 
 const HomePage = () => {
-  const [car, setCar] = useState([]);
+  const itemsPerPage = 4;
+  const [cars, setCars] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const loadCars = async (page) => {
+    setLoading(true);
+    try {
+      const fetchedCars = await fetchAllCars();
+      const totalPages = Math.ceil(fetchedCars.length / itemsPerPage);
+      setTotalPages(totalPages);
+      setCars((prevCars) => [
+        ...prevCars,
+        ...fetchedCars.slice((page - 1) * itemsPerPage, page * itemsPerPage),
+      ]);
+    } catch (error) {
+      console.error("Error fetching", error);
+      setError(true);
+      toast.error("Ooooops something went wrong, please reload the page 😞");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchCar = async () => {
-      try {
-        const car = await fetchAllCars();
-        setCar(car);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching", error);
-        toast.error("Ooooops something went wrong, please reload the page 😞");
-      }
-    };
-    fetchCar();
-  }, []);
+    loadCars(currentPage);
+  }, [currentPage]);
 
   if (loading) {
     return <p>Loading....</p>;
   }
 
   if (error) {
-    return toast.error({ error });
+    return <p>Error loading cars.</p>;
   }
+
+  //   const startPage = (currentPage - 1) * itemsPerPage;
+  //   const currentItems = cars.slice(startPage, startPage + itemsPerPage);
+  //   const totalPages = Math.ceil(cars.length / itemsPerPage);
 
   return (
     <div>
-      <CarCard items={car} />
+      {cars.map((car) => (
+        <CarCard key={car.id} item={car} />
+      ))}
+      {currentPage < totalPages && (
+        <button
+          className={css.btnLoad}
+          onClick={() => setCurrentPage(currentPage + 1)}
+        >
+          Load more
+        </button>
+      )}
     </div>
   );
 };
