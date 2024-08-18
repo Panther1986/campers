1d0d6bd6a7dad1afa5297922159623e071041ec1
Olha Meshcheriakova <polzovatel77@gmail.com>
2024-08-18 23:34:54 +0300

    update13

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
+import css from "./Navigation.module.css";
 
 const buildLinkClass = ({ isActive }) => {
   return clsx(css.link, isActive && css.active);
@@ -7,7 +8,7 @@ const buildLinkClass = ({ isActive }) => {
 
 const Navigation = () => {
   return (
-    <nav>
+    <nav className={css.nav}>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/catalog">Catalog</NavLink>
       <NavLink to="/favorites">Favorites</NavLink>
